12f56f95b8d96c9095abb6c71bdcc0b762210883
Mohamed BEN YOUSSEF <moh.benysf@gmail.com>
2020-09-16 22:29:46 +0200

    use mock only in dev env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpMokRequestInterceptor } from './interceptors/http-mok-request.interceptor';
+import { HttpRequestInterceptor } from './interceptors/http-request.interceptor';
 
+export const isMock = environment.mock;
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: isMock ? HttpMokRequestInterceptor : HttpRequestInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
